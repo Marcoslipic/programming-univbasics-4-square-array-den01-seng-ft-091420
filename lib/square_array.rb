b9dfe695cc443d362a32481fa5994a1f8693280d
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-24 15:19:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  # your code here 
+  new_array = []
+  counter = 0 
+  while array[counter] do
+    new_array = array.push(array[counter] ** 2) 
+    counter += 1 
+ end
+ binding.pry 
+  return new_array 
 end
