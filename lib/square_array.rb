be30d03957a16692c9644c2b2a942b81a9a3f72f
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-24 15:26:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,6 +5,6 @@ def square_array(array)
   new_array = []
  # counter = 0 
   array.length.times { |counter|
-  new_array = array.push(array[counter] ** 2) }
+  new_array = array[counter] ** 2 }
   new_array
 end
